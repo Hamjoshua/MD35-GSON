49c2705a57a8a985b3c5b898a013fff3b9b8812d
Hamjoshua <biryukov-10@list.ru>
2024-10-28 14:00:10 +0700

    Доделано

--- app/src/main/java/com/example/gson/Adapter.kt
+++ app/src/main/java/com/example/gson/Adapter.kt
@@ -1,20 +1,29 @@
 package com.example.gson
 
+import android.R.attr.label
+import android.R.attr.text
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class Adapter(private val list: ArrayList<Photo>) :
-        RecyclerView.Adapter<Adapter.ViewHolder>(){
+
+class ImageAdapter(private val context: Context,
+              private val list: ArrayList<Photo>) :
+        RecyclerView.Adapter<ImageAdapter.ViewHolder>(){
             class ViewHolder(view: View): RecyclerView.ViewHolder(view){
                 val imageView = itemView.findViewById<ImageView>(R.id.rImage)
             }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.r_item,
+        val view = LayoutInflater.from(context).inflate(R.layout.r_item,
             parent, false);
         return ViewHolder(view);
     }
@@ -23,6 +32,13 @@ class Adapter(private val list: ArrayList<Photo>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val photo = list[position];
-        holder.imageView.setImageURI()
+        Glide.with(context).load(photo.getUri()).into((holder.imageView))
+        holder.imageView.setOnClickListener{
+            val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE)
+                    as ClipboardManager
+            val clipData = ClipData.newPlainText("Copied Text", photo.getUri().toString())
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(context, photo.getUri().toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/gson/MainActivity.kt
+++ app/src/main/java/com/example/gson/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.example.gson
 
+import android.net.Uri
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import timber.log.Timber
 import java.net.HttpURLConnection
 import java.net.URL
+
+
+private var RView : RecyclerView? = null
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gridLayoutManager = GridLayoutManager(this, 2)
 
         Timber.plant(Timber.DebugTree())
+
+        RView = findViewById<RecyclerView>(R.id.rView);
+
+        RView?.setHasFixedSize(true)
+        RView?.layoutManager = gridLayoutManager
 
         HttpGetRequest().execute("https://api.flickr.com/services/rest/" +
                 "?method=flickr.photos.search&api_key" +
                 "=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+
+
     }
 }
 
@@ -34,6 +48,11 @@ class HttpGetRequest : AsyncTask<String, Void, String>(){
         val gson = Gson();
         val wrapper: Wrapper = gson.fromJson(result, Wrapper::class.java);
         var counter: Int = 1
+
+        if(RView?.context != null){
+            RView?.adapter = ImageAdapter(RView?.context!!, wrapper.photos.photo);
+        }
+
 
         for (photo in wrapper.photos.photo){
             if(counter % 5 == 0){
@@ -54,7 +73,14 @@ data class Photo(val id: String,
                  val ispublic: Int,
                  val isfriend: Int,
                  val isfamily: Int){
-    
+    fun getUri() : Uri{
+        val address = "https://farm${this.farm}.staticflickr.com" +
+                "/${this.server}/${this.id}_${this.secret}_z.jpg"
+
+        val uri: Uri = Uri.parse(address)
+
+        return uri;
+    }
 }
 
 data class PhotoPage(val photo: ArrayList<Photo>,
