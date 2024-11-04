e47cd50e7e344ed0a4490c2a981fca4125f1dbd8
Hamjoshua <biryukov-10@list.ru>
2024-11-04 20:14:13 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/gson/MainActivity.kt
+++ app/src/main/java/com/example/gson/MainActivity.kt
@@ -11,9 +11,8 @@ import timber.log.Timber
 import java.net.HttpURLConnection
 import java.net.URL
 
-
-private var RView : RecyclerView? = null
 class MainActivity : AppCompatActivity() {
+    public lateinit var RView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,57 +22,61 @@ class MainActivity : AppCompatActivity() {
 
         RView = findViewById<RecyclerView>(R.id.rView);
 
-        RView?.setHasFixedSize(true)
-        RView?.layoutManager = gridLayoutManager
+        RView.setHasFixedSize(true)
+        RView.layoutManager = gridLayoutManager
 
-        HttpGetRequest().execute("https://api.flickr.com/services/rest/" +
-                "?method=flickr.photos.search&api_key" +
-                "=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
-
-
-    }
-}
-
-class HttpGetRequest : AsyncTask<String, Void, String>(){
-    override fun doInBackground(vararg params: String?): String {
-        var text: String = "";
-        val url: URL = URL(params[0])
-        val urlConn: HttpURLConnection = url.openConnection() as HttpURLConnection
-        text = urlConn.inputStream.bufferedReader().readText()
-
-        return text;
+        HttpGetRequest().execute(
+            "https://api.flickr.com/services/rest/" +
+                    "?method=flickr.photos.search&api_key" +
+                    "=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
+        )
     }
 
-    override fun onPostExecute(result: String?) {
-        val gson = Gson();
-        val wrapper: Wrapper = gson.fromJson(result, Wrapper::class.java);
-        var counter: Int = 1
+    inner class HttpGetRequest : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            var text: String = "";
+            val url: URL = URL(params[0])
+            val urlConn: HttpURLConnection = url.openConnection() as HttpURLConnection
+            text = urlConn.inputStream.bufferedReader().readText()
 
-        if(RView?.context != null){
-            RView?.adapter = ImageAdapter(RView?.context!!, wrapper.photos.photo);
+            return text;
         }
 
+        override fun onPostExecute(result: String?) {
+            val gson = Gson();
+            val wrapper: Wrapper = gson.fromJson(result, Wrapper::class.java);
+            var counter: Int = 1
 
-        for (photo in wrapper.photos.photo){
-            if(counter % 5 == 0){
-                Timber.i(photo.toString());
+            if (RView.context != null) {
+                RView.adapter = ImageAdapter(RView.context!!, wrapper.photos.photo);
             }
 
-            ++counter;
+
+            for (photo in wrapper.photos.photo) {
+                if (counter % 5 == 0) {
+                    Timber.i(photo.toString());
+                }
+
+                ++counter;
+            }
         }
     }
+
 }
 
-data class Photo(val id: String,
-                 val owner: String,
-                 val secret: String,
-                 val server: String,
-                 val farm: Int,
-                 val title: String,
-                 val ispublic: Int,
-                 val isfriend: Int,
-                 val isfamily: Int){
-    fun getUri() : Uri{
+
+data class Photo(
+    val id: String,
+    val owner: String,
+    val secret: String,
+    val server: String,
+    val farm: Int,
+    val title: String,
+    val ispublic: Int,
+    val isfriend: Int,
+    val isfamily: Int
+) {
+    fun getUri(): Uri {
         val address = "https://farm${this.farm}.staticflickr.com" +
                 "/${this.server}/${this.id}_${this.secret}_z.jpg"
 
@@ -83,11 +86,13 @@ data class Photo(val id: String,
     }
 }
 
-data class PhotoPage(val photo: ArrayList<Photo>,
-                     val page: Int,
-                     val pages: Int,
-                     val total: Int,
-                     val perpage: Int){
+data class PhotoPage(
+    val photo: ArrayList<Photo>,
+    val page: Int,
+    val pages: Int,
+    val total: Int,
+    val perpage: Int
+) {
 
 }
 
