2fc76f44d5adc784f668aaa4f92d48d3f1c1305c
Hamjoshua <biryukov-10@list.ru>
2024-11-12 10:05:32 +0700

    Запрос теперь через коротину

--- app/src/main/java/com/example/gson/MainActivity.kt
+++ app/src/main/java/com/example/gson/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.gson
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,7 +7,11 @@ import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import timber.log.Timber
 import java.net.HttpURLConnection
 import java.net.URL
@@ -24,14 +27,30 @@ class MainActivity : AppCompatActivity() {
 
         Timber.plant(Timber.DebugTree())
 
-        HttpGetRequest().execute("https://drive.google.com/u/0/uc?id=1-KO" +
-                "-9GA3NzSgIc1dkAsNm8Dqw0fuPxcR&export=download")
-
         RView = findViewById<RecyclerView>(R.id.r_view);
-        RView?.layoutManager = LinearLayoutManager(this)
+        RView.layoutManager = LinearLayoutManager(this)
 
         val btn: Button = findViewById<Button>(R.id.btn_search);
         val editText: EditText = findViewById<EditText>(R.id.search_edit);
+
+        CoroutineScope(Dispatchers.IO).launch {
+            var text: String = "";
+            val url: URL = URL("https://drive.google.com/u/0/uc?id=1-KO" +
+                    "-9GA3NzSgIc1dkAsNm8Dqw0fuPxcR&export=download")
+            val urlConn: HttpURLConnection = url.openConnection() as HttpURLConnection
+            text = urlConn.inputStream.bufferedReader().readText()
+
+            val gson = Gson();
+            val itemType = object : TypeToken<ArrayList<Contact>>() {}.type
+            contacts = gson.fromJson<ArrayList<Contact>>(text, itemType);
+            editableContacts = contacts.clone() as ArrayList<Contact>
+
+            withContext(Dispatchers.Main) {
+                if(RView.context != null){
+                    RView.adapter = CardAdapter(editableContacts);
+                }
+            }
+        }
 
         btn.setOnClickListener{
             val line: String = editText.text.toString()
@@ -42,31 +61,8 @@ class MainActivity : AppCompatActivity() {
             } else{
                 editableContacts = contacts.clone() as ArrayList<Contact>;
             }
-            RView?.adapter = CardAdapter(editableContacts);
+            RView.adapter = CardAdapter(editableContacts);
         }
-    }
-
-    inner class HttpGetRequest : AsyncTask<String, Void, String>(){
-        override fun doInBackground(vararg params: String?): String {
-            var text: String = "";
-            val url: URL = URL(params[0])
-            val urlConn: HttpURLConnection = url.openConnection() as HttpURLConnection
-            text = urlConn.inputStream.bufferedReader().readText()
-
-            return text;
-        }
-
-        override fun onPostExecute(result: String?) {
-            val gson = Gson();
-            val itemType = object : TypeToken<ArrayList<Contact>>() {}.type
-            contacts = gson.fromJson<ArrayList<Contact>>(result, itemType);
-            editableContacts = contacts.clone() as ArrayList<Contact>
-
-            if(RView.context != null){
-                RView.adapter = CardAdapter(editableContacts);
-            }
-        }
-
     }
 }
 
